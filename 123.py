2d450ef485b39cfae74f7a6d7276885f757891e0
ssanek52 <pipidosa@gmail.com>
2024-10-18 20:17:55 +0300

    Add files via upload

---
+++ 123.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as t
+import numpy as np
+
+x = [np.random.uniform(),np.random.uniform(),np.random.uniform(),np.random.uniform(),np.random.uniform(),np.random.uniform(),np.random.uniform(),np.random.uniform(),np.random.uniform(),np.random.uniform()]
+y = [np.random.uniform(),np.random.uniform(),np.random.uniform(),np.random.uniform(),np.random.uniform(),np.random.uniform(),np.random.uniform(),np.random.uniform(),np.random.uniform(),np.random.uniform()]
+t.scatter(x,y)
+t.show()
