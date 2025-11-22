3ba6ae21217d44832f6db3053b8c6fbbfba5af6f
Sirikid <danymalcevthebest98@gmail.com>
2025-11-23 00:51:35 +0300

    add files

--- Planet/model_cache_test.cpp
+++ Planet/model_cache_test.cpp
@@ -1,15 +1,15 @@
 #include <QCoreApplication>
 #include <QDebug>
-#include <unistd.h>
+#include <windows.h>
+#include <psapi.h>
 #include <fstream>
 #include "ModelHandler.h"
 
+// --- Получение RSS под Windows ---
 static size_t currentRSSBytes() {
-    std::ifstream statm("/proc/self/statm");
-    size_t size = 0, resident = 0;
-    if (statm >> size >> resident) {
-        const long pageSize = sysconf(_SC_PAGESIZE);
-        return static_cast<size_t>(resident) * static_cast<size_t>(pageSize);
+    PROCESS_MEMORY_COUNTERS info;
+    if (GetProcessMemoryInfo(GetCurrentProcess(), &info, sizeof(info))) {
+        return static_cast<size_t>(info.WorkingSetSize); // RSS аналог
     }
     return 0;
 }
@@ -17,7 +17,9 @@ static size_t currentRSSBytes() {
 int main(int argc, char** argv) {
     QCoreApplication app(argc, argv);
 
-    const QString modelPath = (argc > 1) ? QString::fromLocal8Bit(argv[1]) : QStringLiteral("Planet/tree.obj");
+    const QString modelPath =
+        (argc > 1) ? QString::fromLocal8Bit(argv[1])
+        : QStringLiteral("Planet/tree.obj");
 
     const size_t rssBefore = currentRSSBytes();
     auto first = ModelHandler::loadShared(modelPath);
@@ -32,9 +34,10 @@ int main(int argc, char** argv) {
 
     qDebug() << "Model loaded" << modelPath;
     qDebug() << "Pointers equal:" << (first == second);
+
     qDebug() << "RSS KB before:" << rssBefore / 1024
-             << "after first:" << rssAfterFirst / 1024
-             << "after second:" << rssAfterSecond / 1024;
+        << "after first:" << rssAfterFirst / 1024
+        << "after second:" << rssAfterSecond / 1024;
 
     return 0;
 }
