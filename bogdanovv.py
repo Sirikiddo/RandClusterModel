6012e3fa650f2497a27369f217b20cb7a0227c70
Sirikiddo <122103602+Sirikiddo@users.noreply.github.com>
2025-11-23 21:23:46 +0300

    Delete bogdanovv.py

--- bogdanovv.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-plt.plot([1,1,2,2,1],[1,2,2,1,1])
-a=set()
-for i in range(10):
-    plt.plot(((1+np.random.uniform())),(1+np.random.uniform()), 'ro')
-plt.show()
